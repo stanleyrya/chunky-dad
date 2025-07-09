36ac4457823e7a310129589771ce6c44cba24d40
Ryan Stanley <stanleyrya@gmail.com>
2025-07-08 23:38:23 -0400

    Update calendar-loader.js

--- js/calendar-loader.js
+++ js/calendar-loader.js
@@ -81,8 +81,7 @@ class CalendarEventsLoader {
 
     // Enhanced event parsing using calendar fields as primary source
     parseEventData(calendarEvent) {
-        this.log("calendar event");
-        this.log(calendarEvent);
+        this.log("calendar event", calendarEvent);
         try {
             // Start with Google Calendar native fields as primary source
             const eventData = {
